360c6a77bec44383cf2bb86c237dbb9b5bcaef07
Pohorielova <anna.pogorielova@gmail.com>
2023-05-27 00:06:20 +0200

    fix

--- db/modelOrder.js
+++ db/modelOrder.js
@@ -3,27 +3,22 @@ const { Schema, model } = require("mongoose");
 const orderSchema = new Schema({
   cartItems: {
     type: Array,
-    required: true,
   },
   address: {
     type: String,
     minlength: 3,
     maxlength: 30,
-    required: true,
   },
   name: {
     type: String,
     minlength: 3,
     maxlength: 30,
-    required: true,
   },
   email: {
     type: String,
-    required: true,
   },
   phoneNumber: {
     type: String,
-    required: true,
   },
 });
 
