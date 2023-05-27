99ef993efb4e50f2bb808900ab8a2b6555a126ea
Pohorielova <anna.pogorielova@gmail.com>
2023-05-27 12:06:59 +0200

    fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { connectMongo } = require("./db/connections");
 
 const start = async () => {
   await connectMongo();
-  app.listen(process.env.PORT, () => {
+  app.listen((process.env.PORT = 5000), () => {
     console.log(`Server running. Use our API on port: ${process.env.PORT}`);
   });
 };
