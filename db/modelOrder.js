cee34e7c9daa835fecf0226c1f81ece7b20b75c2
Pohorielova <anna.pogorielova@gmail.com>
2023-05-27 14:50:08 +0200

    +

--- db/modelOrder.js
+++ db/modelOrder.js
@@ -3,27 +3,22 @@ const { Schema, model } = require("mongoose");
 const orderSchema = new Schema({
   cartItems: {
     type: Array,
-    require: false,
   },
   address: {
     type: String,
     minlength: 3,
     maxlength: 30,
-    require: false,
   },
   name: {
     type: String,
     minlength: 3,
     maxlength: 30,
-    require: false,
   },
   email: {
     type: String,
-    require: false,
   },
   phoneNumber: {
     type: String,
-    require: false,
   },
 });
 
